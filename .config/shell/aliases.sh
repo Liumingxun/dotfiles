aa29136dde44b0c6a42b8d6077d74552e13b5501
lime <liumingxun@yeah.net>
2025-10-03 10:30:02 +0800

    add 24bitcolor function

--- .config/shell/aliases.sh
+++ .config/shell/aliases.sh
@@ -2,6 +2,10 @@ path(){
     echo "$PATH" | tr ':' '\n' | grep -v '/mnt'
 }
 
+24bitcolor() {
+    curl -s https://gist.githubusercontent.com/Liumingxun/a8fc3578bd88d076b2f9fe3fdc0ead13/raw/1875ff9b84a014214d0ce9d922654bb34001198e/24-bit-color.sh | sh
+}
+
 if uname -r | grep -qi "wsl"; then
     alias o=explorer.exe
 fi
