639ccbdd8878565bdcb10fdc49a9ffbb2c0ce5e4
lime <liumingxun@yeah.net>
2025-10-11 20:39:55 +0800

    add install script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 if command -v stow > /dev/null 2>&1; then
-    stow -R -t $HOME . --no-folding --adopt # -nv
+    stow -R -t $HOME . --no-folding --adopt --ignore=install\.sh --ignore=package-list # -nv
 fi
 
 echo -e "\ncheck if the packages in this file exist:"
