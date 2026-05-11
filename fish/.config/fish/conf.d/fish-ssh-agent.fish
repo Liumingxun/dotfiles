7e092e908b0b0a062e45ac14e366cd48c7aa621f
limx <liumingxun@yeah.net>
2026-05-11 21:22:36 +0800

    reset fish-ssh-agent plugin

--- fish/.config/fish/conf.d/fish-ssh-agent.fish
+++ fish/.config/fish/conf.d/fish-ssh-agent.fish
@@ -3,9 +3,5 @@ if test -z "$SSH_ENV"
 end
 
 if not __ssh_agent_is_started
-    # Remove stale sockets from previously dead agents
-    for sock in ~/.ssh/agent/s.*
-        test -S $sock; and rm -f $sock
-    end
     __ssh_agent_start
 end
