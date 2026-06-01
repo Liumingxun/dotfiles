f4648b85dc48b611cc3c28df81ab1359eb8a3fc0
limx <liumingxun@yeah.net>
2026-06-01 13:53:30 +0800

    disable vim-style keybinding

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,9 +4,13 @@
 
 if status is-interactive
     set -U fish_greeting
-    fish_hybrid_key_bindings
+    # fish_hybrid_key_bindings
     if not ssh-add -l &>/dev/null
         ssh-add ~/.ssh/id_ed25519
     end
+    # if uname -r | string match -qi "*wsl*"
+    #     set -gx GALLIUM_DRIVER d3d12
+    #     set -gx LIBVA_DRIVER_NAME d3d12
+    # end
 end
 
